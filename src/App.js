6d3d54b2b935206f3c732f70a3ca15e343a8ff24
Amitsaig <amitsa1995@gmail.com>
2020-06-09 10:50:43 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
+
 
 function App() {
   return (
