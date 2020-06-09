2e88367097176373d3f344b3b0fd73bd81e79c51
Amitsaig <amitsa1995@gmail.com>
2020-06-09 12:04:05 +0300

    step

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, NavLink, Switch, Route, Link, Redirect } from 'react-router-dom';
 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Router>
+            <Redirect from='/' to='/home'/>
+            <div className='Navbar'>
+            <div>
+            <NavLink
+              to="/home"
+              activeStyle={{
+                fontWeight: "bold",
+                color: "white"
+              }}>
+              Home
+            </NavLink>
+            <NavLink
+              to="/selling"
+              style={{ marginInlineStart: '50px' }}
+              activeStyle={{
+                fontWeight: "bold",
+                color: "white"
+              }}>
+              Sell
+            </NavLink>
+            </div>
+            <NavLink
+              to="/user"
+              className='Sign'
+              
+              activeStyle={{
+                fontWeight: "bold",
+                color: "white"
+              }}>
+              Login \ Sign Up
+            </NavLink>
+            </div>
+          </Router>
       </header>
     </div>
-  );
+    );
 }
 
 export default App;
